0f506c016a638989cf32958f31bac5d7eaa7e539
Michael Matus <michael@matus.at>
2020-04-16 14:35:19 +0200

    Coordinate properties of Point3D are now setters

--- Point3D.cs
+++ Point3D.cs
@@ -11,9 +11,9 @@ namespace Bev.IO.BcrReader
             Z = z;
         }
 
-        public double X { get; private set; }
-        public double Y { get; private set; }
-        public double Z { get; private set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
 
         public override string ToString()
         {
